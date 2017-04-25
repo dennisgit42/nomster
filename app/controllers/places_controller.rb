03760e1ec2a3972e32e157f8ab6467fb3f1fb00d
Dennis Ouyang <den2net@yahoo.com>
2017-04-25 02:07:19 +0000

    Changed database to connect each user to many places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.all.paginate(page: params[:page], per_page: 5)
   end
@@ -9,7 +11,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
