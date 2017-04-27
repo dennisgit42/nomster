9dce5845e3198d42f888937ea5b2663f9372b830
Dennis Ouyang <den2net@yahoo.com>
2017-04-27 02:41:25 +0000

    validating name field first try

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -30,12 +34,16 @@ class PlacesController < ApplicationController
   def update
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render file: Rails.root.join("public", "access_denied.html.erb"), status: :forbidden, layout: false 
+      return render text: "Not allowed", status: :forbidden
+      #return render file: Rails.root.join("public", "access_denied.html.erb"), status: :forbidden, layout: false 
       #text: "You are not the user who created this place. Only user that created the place can make changes.", status: :forbidden)
     end
-
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
