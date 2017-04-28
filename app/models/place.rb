fa065819c3f547987f4c099bcf8bd9a21594e59f
Dennis Ouyang <den2net@yahoo.com>
2017-04-28 22:54:16 +0000

    Can get geocoding on new places added from site, but still has problem with rails console

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :address
-  after_validation :address
+  after_validation :geocode
   validates :name, presence: true, length: {minimum: 4}
   validates :address, :description, presence: true
 end
