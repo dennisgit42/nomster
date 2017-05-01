c6d6c4161da09c29fca4bc7b84b4be695177fb8a
Dennis Ouyang <den2net@yahoo.com>
2017-05-01 23:15:55 +0000

    Previewing the email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "den2net@yahoo.com",
+    mail(to: "dennisouyang48@gmail.com",
           subject: "A comment has been added to place you created.")
   end
 
