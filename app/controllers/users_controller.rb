1752e8abb1d95083a70b493cc2959e356635dfd2
Dennis Ouyang <den2net@yahoo.com>
2017-05-04 21:49:03 +0000

    problems with pagination on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @user_comments = @user.comments.all.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   end
 end
