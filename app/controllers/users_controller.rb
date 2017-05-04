ebdde9312f7c5e51aab864ce23d00b7432c8f14b
Dennis Ouyang <den2net@yahoo.com>
2017-05-04 22:45:50 +0000

    Added places on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @user_comments = @user.comments.all.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    @user_comments = @user.comments.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 6)
   end
 end
