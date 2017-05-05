148ee9f172b3cbf2e8abe6599779250ab5dcdb31
Dennis Ouyang <den2net@yahoo.com>
2017-05-05 01:14:38 +0000

    Added dependent: :destroy to place and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :places
-  has_many :comments
+  has_many :places, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
